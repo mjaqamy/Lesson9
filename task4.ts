bd16f4600a7966ff7de1e4d5f8a0ff26b75d1755
mjaqamy <mjaqamy@gmail.com>
2026-02-15 23:04:52 +0300

    feat: add method myFilter

--- task4.ts
+++ task4.ts
@@ -1,5 +1,15 @@
 // Написать свой метод myFilter в прототипе массивов
 
 const arr = [1, 5, -4, 3, -2, 0];
-
+Array.prototype.myFilter = function <T>(func: (value: T, index: number, array: T[]) => boolean): T[] {
+  // создаём новый массив, куда будем складывать подходящие элементы
+  const result = [];
+  // проходим по каждому элементу массива
+  for (let i = 0; i < this.length; i++) {
+    if (func(this[i], i, this)) {
+      result.push(this[i]); //push() - добавляем элемент массива в result
+    }
+  }
+  return result;
+};
 console.log(arr.myFilter((value) => value > 0));
